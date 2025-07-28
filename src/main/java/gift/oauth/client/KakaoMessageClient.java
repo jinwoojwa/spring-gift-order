da63b38bbf291f0d1d67bb17ad4efa1e26d9e4aa
jinwoojwa <jjw6654@gmail.com>
2025-07-28 17:51:38 +0900

    feat: 카카오 메시지 API 클라이언트 구현

---
+++ src/main/java/gift/oauth/client/KakaoMessageClient.java
@@ -0,0 +1,48 @@
+package gift.oauth.client;
+
+import gift.oauth.config.KakaoOauthProperties;
+import gift.oauth.util.KakaoMessageTemplateMaker;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class KakaoMessageClient {
+
+    private final RestClient kakaoRestClient;
+    private final KakaoOauthProperties kakaoProps;
+    private final KakaoMessageTemplateMaker templateMaker;
+
+    public KakaoMessageClient(
+            @Qualifier("kakaoApiClient") RestClient restClient,
+            KakaoOauthProperties kakaoProps,
+            KakaoMessageTemplateMaker templateMaker
+    ) {
+        this.kakaoRestClient = restClient;
+        this.kakaoProps = kakaoProps;
+        this.templateMaker = templateMaker;
+    }
+
+    public void sendOrderMessage(String accessToken, String message) {
+
+        String templateJson = templateMaker.makeMessageTemplate(message);
+
+        String encoded;
+        try {
+            encoded = "template_object=" + URLEncoder.encode(templateJson, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException("❗ 메시지 인코딩 실패", e);
+        }
+
+        kakaoRestClient.post()
+                .uri(kakaoProps.messageUrl())
+                .header("Authorization", "Bearer " + accessToken)
+                .body(encoded)
+                .retrieve()
+                .toBodilessEntity();
+    }
+}
